f0c32757ff65e53e306bde60be2124816639a235
shamim-dewan <570iamkhan@gmail.com>
2022-02-22 21:35:36 +0600

    hello

---
+++ script.js
@@ -0,0 +1,10 @@
+function calc(){
+    const biri = document.getElementById("br-sell").value;
+    const fm = document.getElementById("fb-sell").value;
+    const dm = document.getElementById("df-sell").value;
+    const nap = document.getElementById("np-sell").value;
+    const output = document.getElementById("br-output").value = biri * 0.716;
+}
+
+
+
